852e7d91ea0b613ac1bf14c8c1f843db62d16520
Sarah Lensing <sarahlensing@gmail.com>
2012-02-12 18:32:22 -0500

    precompress css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Iwrotethisandgotin::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -13,6 +11,7 @@ Iwrotethisandgotin::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.precompile += [ "*.css" ]
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
