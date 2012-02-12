ac0380044816ffd2c6053056fe47a0d949637111
Sarah Lensing <sarahlensing@gmail.com>
2012-02-12 14:11:53 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwrotethisandgotin::Application.config.secret_token = '12e1ef95ca677f4f04c05876f907a6d24e564ce4a460d773adc18fc149b3b85a4dacf19dcbe70d70a3b397b6ff4123e15ec48c13c14af79b716aa3aa5aaffa97'
