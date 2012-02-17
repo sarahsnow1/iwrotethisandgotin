c6abb5fe4789259bd87a707edc8b0c0cf879832b
Sarah Lensing <sarahlensing@gmail.com>
2012-02-17 03:30:14 -0500

    compile assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Iwrotethisandgotin::Application.configure do
   config.assets.precompile += [ "application.css" ]
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
 
   # Generate digests for assets URLs
