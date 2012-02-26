00a8468df300b09b2281d00cb50d63ce0e77cd48
Sarah Lensing <sarahlensing@gmail.com>
2012-02-25 19:24:44 -0500

    only correct user can visit protected views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,59 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:edit, :update, :destroy]
+	before_filter :correct_user, only: [:edit, :update, :destroy]
 
-  def show
-    @user = User.find(params[:id])
-  end
+	private
 
-  def new
-    @user = User.new
-  end
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in." unless signed_in?
+	
+		end
+	end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      flash[:success] = "Welcome to I wrote this and got in!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
 
-  def destroy
-    sign_out
-    redirect_to root_path
-  end
+	public
 
-  def edit
-	  @user = User.find(params[:id])
-  end
+  	def show
+    		@user = User.find(params[:id])
+  	end
 
-  def update
-	  @user = User.find(params[:id])
-	  if @user.update_attributes(params[:user])
-		  flash[:success] = "Profile updated"
-		  redirect_to @user
-	  else
-		  render 'edit'
-	  end
-  end
+  	def new
+    		@user = User.new
+  	end
 
+  	def create
+    		@user = User.new(params[:user])
+   	 	if @user.save
+      			flash[:success] = "Welcome to I wrote this and got in!"
+      			redirect_to @user
+    		else
+      			render 'new'
+    		end
+  	end
+
+  	def destroy
+		sign_out
+    		redirect_to root_path
+  	end
+
+  	def edit
+	 	@user = User.find(params[:id])
+  	end
+
+  	def update
+	  	@user = User.find(params[:id])
+	  	if @user.update_attributes(params[:user])
+		  	flash[:success] = "Profile updated"
+			sign_in @user
+			redirect_to @user
+	  	else
+		  	render 'edit'
+	  	end
+  	end
 end
